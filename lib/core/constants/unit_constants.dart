6abe5588c5756ed0ae6b6ff20b4e3489631dc456
Gemechu Alemu <alemugemechu44@gmail.com>
2026-06-12 09:05:53 +0300

    astu

--- lib/core/constants/unit_constants.dart
+++ lib/core/constants/unit_constants.dart
@@ -1,6 +1,6 @@
 abstract final class UnitConstants {
   static const squareMetersPerHectare = 10000.0;
   static const squareMetersPerAcre = 4046.856;
-  static const squareMetersPerTimad = 2500.0;
+  static const squareMetersPerTimad = 10000.0 / 6.0;
   static const squareMetersPerKert = 25.0;
 }
